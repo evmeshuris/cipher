6f02d33e16450157856391bcc626060113ee1f8e
Evgeniya Meshuris <em@theemm.com>
2022-03-22 11:38:29 -0700

    initial commit
    co-authored-by: Tony Martinez<tony.martinez5897@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,11 @@
-jQuery(document).ready(function() {
-  const sentence = prompt("Enter a sentence:");
-  console.log(sentence);
+.green-background {
+  background-color: green;
+}
 
-  function newString(string) {
-    const char1 = string.charAt(0).toUpperCase();
-    const char2 = string.charAt(sentence.length - 1).toUpperCase();
-    const twoLetters = char2.concat(char1);
-    console.log(twoLetters);
-    return twoLetters
-  };
-  const alltogether = newString(sentence);
-  
-  const final = sentence + alltogether;
-  
-  let length = sentence.length;
-  console.log(length);
-  document.getElementById("demo").innerHTML = length;
-  
-  let divided = (length / 2);
-  divided.toFixed(0);
-  console.log(divided);
+.yellow-background {
+  background-color: yellow;
+}
 
-
-  let char3 = sentence.charAt(divided);
-  console.log(char3);
-  console.log(char3 + final);
-  
-});
-
+.red-background {
+  background-color: red;
+}
